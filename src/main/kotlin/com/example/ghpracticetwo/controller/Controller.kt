57cabe289e1d2096c380d0ea6e2afea418abd16f
Charchit Kasliwal <charchit.kasliwal@justeattakeaway.com>
2023-09-10 21:06:59 +0100

    New Changes added

--- src/main/kotlin/com/example/ghpracticetwo/controller/Controller.kt
+++ src/main/kotlin/com/example/ghpracticetwo/controller/Controller.kt
@@ -1,7 +1,9 @@
 package com.example.ghpracticetwo.controller
 
+import com.example.ghpracticetwo.service.PublicService
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class Controller {
+class Controller(var publicService: PublicService) {
+
 }
